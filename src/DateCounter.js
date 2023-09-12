2aa377ebcf5f236ae712ae2b242154151c74787a
ozge <ozge.belenn@gmail.com>
2023-09-12 21:45:23 +0300

    dispach payload durumu

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+//reducerda payload durumu opsiyaneldir Bunu aşağıdaki gibi yazabilir veya direkt olarak payloadları fonsksiyondan silip +1 _1 ekleyebiliriz.
+// if (action.type === "inc") return state + action.payload;
+//Ancak e.target.value durumda payload girmemiz gerekir aksi takdirde değeri alamaz
+//Buradaki reset durumunu ben tanımladım
+const reducer = (state, action) => {
+  console.log(state, action);
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "setCount") return action.payload;
+  if (action.type === "reset") return 0;
+};
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+  const [count, dispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -9,17 +23,20 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: "dec", payload: -1 });
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    //setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch({ type: "inc", payload: 1 });
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    //setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
+    //setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
@@ -27,7 +44,8 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
+    dispatch({ type: "reset", payload: 0 });
     setStep(1);
   };
 
